74d31c52dc0d7070bde2fa272c171f64a55a49ca
Misha <mishakogyt17@gmail.com>
2020-03-16 15:20:01 +0200

    lesson 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 import './slider';
 import tabs from './modules/tabs';
 import modals from './modules/modals';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () =>{
+"use strict";
+
    modals();
    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');   
    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div ', 'after_click');
-
-
+   forms();
 });
