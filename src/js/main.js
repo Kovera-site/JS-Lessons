12f77d8ae80ccce5d27b650ba21382c1c88d26a2
Misha <mishakogyt17@gmail.com>
2020-03-21 15:37:52 +0200

    lesson 6

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,15 @@ import tabs from './modules/tabs';
 import modals from './modules/modals';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
+
 
 
 window.addEventListener('DOMContentLoaded', () =>{
 "use strict";
 
    let modalState = {};
+   let deadline = '2020-03-27 15:34';
 
    changeModalState(modalState);
    modals();
@@ -16,4 +19,5 @@ window.addEventListener('DOMContentLoaded', () =>{
    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div ', 'after_click');
    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
    forms(modalState);
+   timer('.container1', deadline);
 });
